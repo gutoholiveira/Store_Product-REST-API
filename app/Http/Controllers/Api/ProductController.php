670ac08592777f61ae6573cddfb9d62fd79629c3
Gustavo Oliveira <gutoholiveira@hotmail.com>
2022-02-12 10:48:53 -0300

    First Commit

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Store;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    private $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Store $store)
+    {
+        if(!empty($store->name)){
+            return $store->products;
+        }
+
+        return $this->product->paginate(10);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Store $store)
+    {
+        return $store->products()->create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product  $product)
+    {
+        $product->update($request->all());
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        return $product->delete();
+    }
+}
